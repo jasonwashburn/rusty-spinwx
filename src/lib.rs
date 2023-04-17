65d18a3cd54c9507c5fd8a1514efb7cc58e8ef9d
Jason Washburn <jason.washburn@gmail.com>
2023-04-16 20:30:16 -0500

    parsed idx route working

--- src/lib.rs
+++ src/lib.rs
@@ -28,6 +28,8 @@ mod api {
 
     use std::ops::Sub;
 
+    use crate::s3_utils::build_grib_idx_key;
+
     use super::*;
     use gfs::gfs_run_is_complete;
     use s3_utils::{build_grib_key_prefix, fetch_list_of_grib_keys};
@@ -69,14 +71,12 @@ mod api {
     }
 
     pub fn route_gfs_idx(_req: Request, _params: Params) -> Result<Response> {
-        let mut response: HashMap<String, String> = HashMap::new();
         let now = Utc::now();
         let latest_run = dbg!(gfs::determine_latest_possible_run(now));
         if let Some(latest_run) = latest_run {
             let grib_prefix = build_grib_key_prefix(&latest_run);
             let grib_keys = fetch_list_of_grib_keys(&grib_prefix).unwrap();
             let list_result = s3_utils::parse_list_bucket_result(&grib_keys).unwrap();
-            //response.insert(String::from("grib_keys"), grib_keys);
             match serde_json::to_string(&list_result) {
                 Ok(json) => Ok(http::Response::builder()
                     .status(http::StatusCode::OK)
@@ -89,7 +89,6 @@ mod api {
     }
 
     pub fn route_gfs_idx_info(_req: Request, params: Params) -> Result<Response> {
-        let mut response: HashMap<String, i32> = HashMap::new();
         let year = params
             .get("year")
             .unwrap_or_default()
@@ -115,12 +114,16 @@ mod api {
             .unwrap_or_default()
             .parse::<i32>()
             .unwrap_or_default();
-        response.insert(String::from("year"), year);
-        response.insert(String::from("month"), month);
-        response.insert(String::from("day"), day);
-        response.insert(String::from("hour"), hour);
-        response.insert(String::from("forecast"), forecast);
-        match serde_json::to_string(&response) {
+
+        let idx_key = build_grib_idx_key(year, month, day, hour, forecast);
+        let idx_data = s3_utils::get_s3_object(S3_BUCKET, &idx_key).unwrap();
+        let idx_entries = gfs::parse_idx_file(&idx_data).unwrap();
+        // response.insert(String::from("year"), year);
+        // response.insert(String::from("month"), month);
+        // response.insert(String::from("day"), day);
+        // response.insert(String::from("hour"), hour);
+        // response.insert(String::from("forecast"), forecast);
+        match serde_json::to_string(&idx_entries) {
             Ok(json) => Ok(http::Response::builder()
                 .status(http::StatusCode::OK)
                 .body(Some(json.into()))?),
@@ -146,12 +149,13 @@ mod api {
 }
 
 mod gfs {
-
     use super::NUM_EXPECTED_FORECASTS;
     use crate::s3_utils::{
         build_grib_key_prefix, fetch_list_of_grib_keys, parse_list_bucket_result,
     };
+    use anyhow::Result;
     use chrono::{DateTime, Timelike, Utc};
+    use serde::{Deserialize, Serialize};
 
     pub fn gfs_run_is_complete(run: DateTime<Utc>) -> bool {
         let grib_prefix = build_grib_key_prefix(&run);
@@ -175,6 +179,44 @@ mod gfs {
             .with_minute(0)?
             .with_second(0)?
             .with_nanosecond(0)
+    }
+
+    #[derive(Debug, Serialize, Deserialize, PartialEq)]
+    pub struct IdxEntry {
+        index: i32,
+        start_byte: i32,
+        stop_byte: Option<i32>,
+        model_run: String,
+        parameter: String,
+        level: String,
+        forecast_type: String,
+    }
+
+    pub fn parse_idx_file(idx_data: &str) -> Result<Vec<IdxEntry>> {
+        let mut idx_entries: Vec<IdxEntry> = Vec::new();
+        let mut prev_start_byte: Option<i32> = None;
+        for line in idx_data.lines().rev() {
+            let split_line: Vec<&str> = line.split(':').collect();
+            let index = split_line[0];
+            let start_byte = split_line[1];
+            let model_run = split_line[2].trim_start_matches("d=");
+            let parameter = split_line[3];
+            let level = split_line[4];
+            let forecast_type = split_line[5];
+
+            idx_entries.push(IdxEntry {
+                index: index.parse::<i32>().unwrap(),
+                start_byte: start_byte.parse::<i32>().unwrap(),
+                stop_byte: prev_start_byte,
+                model_run: model_run.to_string(),
+                parameter: parameter.to_string(),
+                level: level.to_string(),
+                forecast_type: forecast_type.to_string(),
+            });
+            prev_start_byte = start_byte.parse::<i32>().ok();
+        }
+        idx_entries.reverse();
+        Ok(idx_entries)
     }
 }
 
@@ -221,6 +263,16 @@ mod s3_utils {
     pub fn fetch_list_of_grib_keys(grib_prefix: &str) -> Result<String> {
         let url = format!("https://{S3_BUCKET}.s3.amazonaws.com/?list-type=2&prefix={grib_prefix}");
         println!("Fetching from URL: {}", url);
+        let mut resp = spin_sdk::outbound_http::send_request(
+            http::Request::builder().method("GET").uri(url).body(None)?,
+        )?;
+        let body = resp.body_mut().take().unwrap();
+        Ok(String::from_utf8_lossy(&body).to_string())
+    }
+
+    pub fn get_s3_object(bucket: &str, key: &str) -> Result<String> {
+        let url = format!("https://{bucket}.s3.amazonaws.com/{key}");
+        println!("Fetching S3 object from URL: {}", url);
         let mut resp = spin_sdk::outbound_http::send_request(
             http::Request::builder().method("GET").uri(url).body(None)?,
         )?;
